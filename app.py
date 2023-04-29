034de53f466dddd6b2d5a7ffad8a60a37208557e
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-04-29 18:20:49 +0200

    Pruebas buscador

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
+import json
 from flask import Flask, render_template, request
 app = Flask(__name__)	
+
+with open('msx.json') as f:
+    juegos = json.load(f)
 
 @app.route('/')
 def inicio():
     return render_template("inicio.html")
 
-@app.route('/juegos')
-def articulos():
-    nombre=request.args.get("nombre")
-    precio=request.args.get("precio")
-    return render_template("juegos.html",nombre=nombre,precio=precio)
-
-@app.route('/acercade')
-def acercade():
-    return render_template("acercade.html")
-
-@app.route('/acercade/<nombre>/<edad>')
-def acercade_con_nombre(nombre,edad):
-    return render_template("acercade.html",nombre=nombre,edad=edad)
-
+@app.route('/juegos',methods=["GET","POST"])
+def juegos():
+    if request.method=="POST":
+        buscador=request.form.get("buscador")
+        try:
+            info="Prueba info".format(buscador)
+        except:
+            info="No hay"
+        return render_template("listajuegos.html",info=info)
+    else:
+        return render_template("juegos.html")
 
 app.run("0.0.0.0",5000,debug=True)
