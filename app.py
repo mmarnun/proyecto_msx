bac21fd86294fa050c0f9f48e3be788df423321c
mmarnun <123729868+mmarnun@users.noreply.github.com>
2023-05-01 18:55:44 +0200

    Buscador y resultado

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)	
 
-with open('msx.json') as f:
-    juegos = json.load(f)
+with open('msx.json') as r:
+    infojuegos = json.load(r)
 
 @app.route('/')
 def inicio():
     return render_template("inicio.html")
 
-@app.route('/juegos',methods=["GET","POST"])
+@app.route('/juegos', methods=['GET', 'POST'])
 def juegos():
-    if request.method=="POST":
-        buscador=request.form.get("buscador")
-        try:
-            info="Prueba info".format(buscador)
-        except:
-            info="No hay"
-        return render_template("listajuegos.html",info=info)
-    else:
-        return render_template("juegos.html")
+    if request.method == 'POST':
+        return redirect(url_for('listajuegos'))
+    return render_template('juegos.html')
 
+@app.route('/listajuegos', methods=['POST'])
+def listajuegos():
+    nombre = request.form.get('buscador', '')
+    juegos = [juego for juego in infojuegos if str(juego['nombre']).lower().startswith(nombre.lower())]
+    return render_template('listajuegos.html', juegos=juegos)
+            
 app.run("0.0.0.0",5000,debug=True)
